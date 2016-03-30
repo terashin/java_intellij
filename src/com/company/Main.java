fafee3c5525236707d84f12509a1900ccbda800a
shinya <asitahaiitennki@gmail.com>
2016-03-30 23:17:45 +0900

    "first commit"

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.prefs.Preferences;
+
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.println("Hello, World");
+    }
+}
